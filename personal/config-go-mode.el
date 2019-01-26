6ac5c8dab30f898e3133c0567e54ac454f0113bc
Karthik Muralidharan <mkarthik91@gmail.com>
2019-01-27 00:58:34 +0530

    cleanup; config mode for go and terraform

--- personal/config-go-mode.el
+++ personal/config-go-mode.el
@@ -1,16 +1,17 @@
-(defun custom-go-mode-hook ()
-  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
-  (setq tab-width 4)
+(with-eval-after-load 'go-mode
+  (defun custom-go-mode-hook ()
+    (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
+    (setq tab-width 4)
 
-  ;; guru settings
-  (go-guru-hl-identifier-mode)                    ; highlight identifiers
-  ;; Key bindings specific to go-mode
-  (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
-  (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
-  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
-  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
-  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
-  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
-  )
+    ;; guru settings
+    (go-guru-hl-identifier-mode)                    ; highlight identifiers
+    ;; Key bindings specific to go-mode
+    (local-set-key (kbd "M-.") 'godef-jump)         ; Go to definition
+    (local-set-key (kbd "M-*") 'pop-tag-mark)       ; Return from whence you came
+    (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+    (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+    (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+    (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
+    )
 
-(add-hook 'go-mode-hook 'custom-go-mode-hook)
+  (add-hook 'go-mode-hook #'custom-go-mode-hook))
--- personal/terraform-mode.el
+++ personal/terraform-mode.el
@@ -3,6 +3,5 @@
 (prelude-require-packages '(terraform-mode company-terraform))
 
 (with-eval-after-load 'terraform-mode
-  (company-terraform-init))
-
-(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+  (company-terraform-init)
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
